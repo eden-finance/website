36dc5c57fa4d9d44f6614d297e78f872ff50c528
phoenix <ayomikuntemitope.araoye@gmail.com>
2025-05-13 15:24:03 +0300

    fix

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import Link from 'next/link'
 import { Button } from 'ui/button'
@@ -6,10 +6,6 @@ import LogoIcon from '../icons/logo'
 import { ThemeToggle } from '../miscellaneous/toggle'
 
 export default function Navbar() {
-
-  function handleRedirectToApp() {
-    window.open('https://app.eden-finance.xyz', '_blank')
-  }
   return (
     <nav className="bg-background/80 sticky top-0 z-50 w-full border-b border-purple-500/10 shadow-sm backdrop-blur-md dark:bg-black/80">
       <div className="px-4 sm:px-6 lg:px-8">
@@ -22,8 +18,30 @@ export default function Navbar() {
           </div>
           <div className="flex gap-2">
             <ThemeToggle />
-            <Button onClick={() => handleRedirectToApp()} className="bg-purple-500 text-white hover:bg-purple-600">
-              Launch App
+            <Button
+              asChild
+              className="bg-purple-500 text-white hover:bg-purple-600"
+            >
+              <Link
+                href="https://app.eden-finance.xyz"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Launch App
+              </Link>
+            </Button>
+            <Button
+              asChild
+              variant="outline"
+              className="border-purple-500 text-purple-500 hover:bg-purple-100 dark:hover:bg-purple-900"
+            >
+              <Link
+                href="https://leaderboard.eden-finance.xyz/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View Leaderboard
+              </Link>
             </Button>
           </div>
         </div>
